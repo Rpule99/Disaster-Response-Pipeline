ee118c33a1b65aebf6a1ca910be5643da2f9ee21
Rpule99 <rethabilepule1997@gmail.com>
2024-05-01 22:58:03 +0200

    add documentations to scripts again

--- data/data_clean.py
+++ data/data_clean.py
@@ -3,7 +3,7 @@ import pandas as pd
 #remove Duplicates
 def Duplicate_clean(df):
     '''  
-     1. removed doplicated rows and returns cleaned DF
+     1. removed duplicated rows and returns cleaned DF
     '''
     if df.duplicated().sum() > 0:
         df.drop_duplicates(inplace=True)
