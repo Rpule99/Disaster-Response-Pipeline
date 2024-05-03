ee0b4c9e02e967e20855c555cb3135f8ca9c0a24
Rpule99 <rethabilepule1997@gmail.com>
2024-05-03 04:19:56 +0200

    Added line to remove related = 2

--- data/data_clean.py
+++ data/data_clean.py
@@ -4,9 +4,11 @@ import pandas as pd
 def Duplicate_clean(df):
     '''  
      1. removed duplicated rows and returns cleaned DF
+     2. remove where related = 2
     '''
     if df.duplicated().sum() > 0:
         df.drop_duplicates(inplace=True)
     else:
         pass
+    df = df[df['related'] != 2]
     return df
